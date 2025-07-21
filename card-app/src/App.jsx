56a98f1a9f459cf6687435d4c94f1a83cf776867
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-07-22 01:53:38 +0530

    Code of the day

---
+++ card-app/src/App.jsx
@@ -0,0 +1,18 @@
+import Card from './Components/card'
+import "./App.css"
+import Albert_Einstein from './assets/Albert-Einstein.jpg'
+import Issac_Newton from './assets/isac_newton.avif'
+import Madam_Curie from './assets/madam_curie.jpg'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <Card name="Albert Einstein" image={Albert_Einstein} disc = "disc 1"/>
+      <Card name="Issac Newton" image={Issac_Newton} disc = "disc 2"/>
+      <Card name="Madam Curie (Marie Curie)" image={Madam_Curie} disc = "disc 3"/>
+    </div>
+  )
+}
+
+export default App
