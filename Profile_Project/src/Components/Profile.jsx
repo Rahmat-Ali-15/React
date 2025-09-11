1c837ab1eb82b2b41b6547187d83256c61010144
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-09-11 19:58:01 +0530

    Updated code

--- Profile_Project/src/Components/Profile.jsx
+++ Profile_Project/src/Components/Profile.jsx
@@ -32,7 +32,7 @@
 //                                     <div key={elss.rate} className="box-1">
 //                                         <h2>{elss.rate}</h2>
 //                                         <p>{elss.text1}</p>
-//                                         <div style={{ background: elss.color, padding: "10px 20px", borderRadius: "10px" }} className='img-ss'>
+//                                         <div style={{ background: elss.color, padding: "10px 20px", borderRadius: "10px" }}  className='img-ss'>
 //                                             <img style={{ background: elss.color }} src={elss.icon} alt={elss.text} />
 //                                             <p style={{ background: elss.color }}>{elss.text}</p>
 //                                         </div>
@@ -56,33 +56,44 @@ export const Profile = () => {
         <>
             {
                 data.profile.map((el) => (
+                    <body>
 
-                    <main key={el.name}>
-                        <section className="profile-container">
-                            <div className="info-div">
-                                <h3 name="profile_name">Name : {el.name}</h3>
-                                <p name="designation">Profile : {el.designation}</p>
-                                <p name="skill">Skills : {el.skill}</p>
-                                <div className="platform-div" style={{ "display": "flex" }}>
-                                    <p>Platform : </p>
-                                    {el.platform.map((pl) => (
-                                        <p key={pl.name} style={{ "display": "flex", "gap": "20px", "color": "red", "border": "1px solid red", "padding": "10px" }}>
-                                            {pl.name}
-                                        </p>
-                                    ))}
+                        <main key={el.name}>
+                            <section className="profile-container">
+                                <div className="info-div">
+                                    <h3 name="profile_name">Name : {el.name}</h3>
+                                    <p name="designation" id="profile">Profile : {el.designation}</p>
+                                    <div className="skill-div">
+                                        <p name="skill" id="skill">Skills : </p>
+                                        <div>
+                                            {el.skill.map((sk) => (
+                                                <p key={sk}>{sk}</p>
+                                            ))}
+                                        </div>
+                                    </div>
+                                    <div className="platform-div">
+                                        <p>Platform : </p>
+                                        <div>
+                                            {el.platform.map((pl) => (
+                                                <p id="platform_p" key={pl.name} style={{ "color": pl.color }}>{pl.name}</p>
+                                            ))}
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className="profile-img">
+                                <div className="profile-img">
+                                    <img src={el.img} alt={el.name} />
+                                </div>
+                            </section>
 
-                            </div>
-                        </section>
+                            <section className="socialMedia-container">
+                                    <div className="fan"></div>
+                                    <div className="follower"></div>
+                                    <div className="following"></div>
+                            </section>
 
-                        <section className="socialMedia-container">
-
-                        </section>
-
-                    </main>
+                        </main>
+                    </body>
                 ))
             }
         </>
