ce8c628ee1e0816e3b9ad478255274e4787d9685
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-09-15 20:09:38 +0530

    Modified code

--- Profile_Project/src/App.jsx
+++ Profile_Project/src/App.jsx
@@ -1,16 +1,3 @@
-// import './App.css'
-// import { Profile } from './Components/Profile';
-// function App() {
-
-//   return (
-//     <>
-//       <Profile />
-//     </>
-//   )
-// }
-
-// export default App
-
 import { Profile } from "./Components/Profile";
 
 export function App() {
