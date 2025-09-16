8369715d7ea5e7165c4d5e99f7df909ad46da34e
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-09-16 22:22:35 +0530

    Modified code

--- Todo/src/Components/ListTodo.jsx
+++ Todo/src/Components/ListTodo.jsx
@@ -4,13 +4,13 @@ export const ListTodo = ({ props }) => {
     console.log(props)
     return (
         <>
-            <h1>List Todo</h1>
+            <h1 style={{"color": "white"}}>List Todo</h1>
 
             {
                 props.map((el) => (
-                    <div key={el.data} >
-                        <p>{el.text}</p>
-                        <div style={{"display" : "flex", "flexDirection": "column",  "width": "200px"}}>
+                    <div key={el.data} style={{"display" : "flex","justifyContent":"center","gap" : "100px", "width": "100%"}}>
+                        <p style={{"color": "white"}}>{el.text}</p>
+                        <div style={{"display" : "flex",  "width": "200px","gap" : "15px"}}>
                             <button>Edit</button>
                             <button>Delete</button>
                         </div>
