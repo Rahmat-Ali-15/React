f1d0f2d16be38aa8b000c894df7bbaef8724eb46
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-09-16 21:55:58 +0530

    Modified code

--- Todo/src/Components/AddTodo.jsx
+++ Todo/src/Components/AddTodo.jsx
@@ -4,37 +4,31 @@ import "../Components/AddTodo.css"
 
 export const AddTodo = () => {
 
-    const [data, setData] = React.useState([]);
-    const [text, setText] = React.useState("");
+  const [data, setData] = React.useState([]);
+  const [text, setText] = React.useState("");
 
-    const handleFunck = () => {
-        if(text.trim() === "") return;
+  const handleFunck = () => {
+    if (text.trim() === "") return;
 
-        let todo = {
-            id : Date.now(),
-            text,
-            isEdited : false,
-            isCheck : false
-        }
-
-        setData([...data, todo]);
-        localStorage("todo", JSON.stringify("setData"))
+    let todo = {
+      id: Date.now(),
+      text,
+      isEdited: false,
+      isCheck: false
     }
+
+    setData([...data, todo]);
+    localStorage("todo", JSON.stringify("setData"))
+  }
 
   return (
     <>
-    <div style={{ 
-      "height" : "100vh",
-      "width": "100%",
-      "display" : "flex",
-      "flexDirection":"column",
-      "alignItems" : "center"
-    }}>
+      <div className='addTodo-container'>
         <h1>Todo</h1>
-        <input onChange={(e)=> setText(e.target.value)} type="text" placeholder='Enter the todo' />
+        <input onChange={(e) => setText(e.target.value)} type="text" placeholder='Enter the todo' />
         <input onClick={handleFunck} type="button" value="Add Todo" />
-    </div>
-      <ListTodo props = {data} />
+      </div>
+      <ListTodo props={data} />
     </>
   )
 }
