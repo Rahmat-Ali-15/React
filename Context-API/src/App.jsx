cce166e6804dfe5ab32df6d4d74fb386bb724700
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-09-25 14:01:58 +0530

    Code of the day

---
+++ Context-API/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Counter } from './Components/Counter'
+import { Dummy } from './Components/Dummy'
+
+export function App() {
+
+  return (
+    <>
+      <Counter />
+      <Dummy />
+    </>
+  )
+}
