e71c0609a5c326793cd50aa4619b651646460c6f
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-09-30 16:26:18 +0530

    Code of the day

---
+++ Axiox/src/Components/ApiCall.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import axios from "axios"
+
+const getData = async(api) => {
+
+    try {
+        let respond = await axios.get(api);
+        console.log(respond.data)
+    } catch (error) {
+        console.log(error)
+    }
+
+    
+}
+
+export const ApiCall = () => {
+
+    let url = `https://jsonplaceholder.typicode.com/todos`;
+
+    React.useEffect(() => {
+        getData(url)
+    }, []);
+
+    return(
+        <>
+            <h1>ApiCall</h1>
+        </>
+    )
+}
