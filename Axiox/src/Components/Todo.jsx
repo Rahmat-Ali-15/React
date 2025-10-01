41f475fb043b21e7fa6e8d037780218a8ea1afcc
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-10-01 09:18:11 +0530

    Code of the day

--- Axiox/src/Components/Todo.jsx
+++ Axiox/src/Components/Todo.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
-import { useRef } from "react";
+import { useState,useRef } from "react";
 import {nanoid} from "nanoid"
+import { TodoList } from "./TodoList";
 
 export const Todo = () => {
+
+    const [count, setCount] = useState(0)
 
     // let api = `https://api-server-zecj.onrender.com/todo`;
     let api = `https://api-database-1.onrender.com/Todo`;
@@ -20,7 +23,8 @@ export const Todo = () => {
         }
 
         try {
-            let res = await axios.post(api,obj)
+            let res = await axios.post(api,obj);
+            setCount((prev) => prev + 1)
             console.log(res)
         } catch (error) {
             console.log(error)
@@ -33,6 +37,7 @@ export const Todo = () => {
             <h1>Todo</h1>
             <input type="text" name="addTodo" id="" placeholder="add todo..." ref={dataRef} />
             <input type="button" value="Add Todo" onClick = {handleClick} />
+            <TodoList props={count} />
         </>
     )
 }
