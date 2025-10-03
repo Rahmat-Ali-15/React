aaab0d0b3ffd7e6fea721da890f8235889124b20
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-10-03 10:25:30 +0530

    Added logic for delete button

--- Todo/src/Components/AddTodo.jsx
+++ Todo/src/Components/AddTodo.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import { ListTodo } from './ListTodo'
-import "../Components/AddTodo.css"
+import React from "react";
+import { ListTodo } from "./ListTodo";
+import "../Components/AddTodo.css";
 
 export const AddTodo = () => {
-
   const [data, setData] = React.useState([]);
   const [text, setText] = React.useState("");
 
@@ -14,23 +13,38 @@ export const AddTodo = () => {
       id: Date.now(),
       text,
       isEdited: false,
-      isCheck: false
-    }
+      isCheck: false,
+    };
 
     setData([...data, todo]);
-    localStorage("todo", JSON.stringify("setData"))
+    localStorage.setItem("todoData", JSON.stringify([...data, todo]));
+  };
+
+   const handleDel = (id) => {
+      const deleteItem = data.filter((el) => el.id !== id)
+      setData(deleteItem);
+      localStorage.setItem("todoData", JSON.stringify(deleteItem))
   }
 
   return (
     <>
-      <div className='addTodo-container'>
-        <h1 style={{"color": "white"}}>Todo</h1>
+      <div className="addTodo-container">
+        <h1>Todo</h1>
         <div>
-          <input onChange={(e) => setText(e.target.value)} type="text" placeholder='Enter the todo' />
-          <button onClick={handleFunck}><span class="text">Add Todo</span></button>
+          <input
+            onChange={(e) => setText(e.target.value)}
+            type="text"
+            placeholder="Enter the todo"
+          />
+          <button
+            className="todo-btn"
+            onClick={handleFunck}
+          >
+            <span className="text">Add Todo</span>
+          </button>
         </div>
       </div>
-      <ListTodo props={data} />
+      <ListTodo props={data} handleDel={handleDel} />
     </>
-  )
-}
+  );
+};
