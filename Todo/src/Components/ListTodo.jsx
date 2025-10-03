d16d88f9d111b97d245424ac6b9c5be3fb06fb98
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-10-03 10:27:25 +0530

    Modified code

--- Todo/src/Components/ListTodo.jsx
+++ Todo/src/Components/ListTodo.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import React from "react";
 
-export const ListTodo = ({ props }) => {
-    console.log(props)
-    return (
-        <>
-            <h1 style={{"color": "white"}}>List Todo</h1>
+import "../Components/ListTodo.css";
 
-            {
-                props.map((el) => (
-                    <div key={el.data} style={{"display" : "flex","justifyContent":"center","gap" : "100px", "width": "100%"}}>
-                        <p style={{"color": "white"}}>{el.text}</p>
-                        <div style={{"display" : "flex",  "width": "200px","gap" : "15px"}}>
-                            <button>Edit</button>
-                            <button>Delete</button>
-                        </div>
-                    </div>
-                ))
-            }
-        </>
-    )
-}
+import { MdDeleteForever } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
+import { FaRegCheckCircle } from "react-icons/fa";
+import { GoCheckCircleFill } from "react-icons/go";
+
+export const ListTodo = ({ props, handleDel }) => {
+  // console.log(props)
+
+  return (
+    <>
+      <h1 style={{ color: "white" }}>Todo List</h1>
+
+      {props.map((el, i) => (
+        <div
+          className="list-todo-container"
+          key={i}
+        >
+          <div className="check-text-div">
+            <FaRegCheckCircle className="check-icon" />
+            <GoCheckCircleFill className="fill-check-icon" />
+            <p className="input-text">{el.text}</p>
+          </div>
+          <div className="edit-del-div">
+            <MdEdit className="edit-icon" />
+            <MdDeleteForever
+              className="del-icon"
+              onClick={()=>handleDel(el.id)}
+            />
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
