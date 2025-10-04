56ed4fe192bbfd94e92ced2f8227d37674490246
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-10-04 08:03:45 +0530

    Created todoList using useRef hook

---
+++ Todo-useState/src/Components/TodoList.jsx
@@ -0,0 +1,26 @@
+import { FaRegCircleCheck } from "react-icons/fa6";
+import { MdEdit } from "react-icons/md";
+import { MdDeleteForever } from "react-icons/md";
+
+import "../App.css"
+
+export const TodoList = ({ data, handleDel }) => {
+  return (
+    <>
+      <div className="container" >
+        {data.map((el) => (
+          <div key={el.id} className="todo-list-container">
+            <div>
+              <FaRegCircleCheck className="check-icon" />
+              <p>{el.text}</p>
+            </div>
+            <div style={{ display: "flex" }}>
+              <MdEdit className="edit-icon" />
+              <MdDeleteForever className="del-icon" onClick={()=> handleDel(el.id)} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
