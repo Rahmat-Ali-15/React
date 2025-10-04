c08b4d47a6b2bbcb785768e9a7bf0e7cc6048aac
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-10-04 08:03:09 +0530

    Created todo app using useRef hook

---
+++ Todo-useState/src/Components/AddTodo.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+import "../App.css";
+import { TodoList } from "./TodoList";
+
+export const AddTodo = () => {
+  const [input, setInput] = useState("");
+
+  const [data, setData] = useState([]);
+
+  const handleAddTodo = () => {
+    if (input.trim() === "") return;
+
+    const todo = {
+      id: Date.now(),
+      text: input,
+      isEdited: false,
+      isCompleted: false,
+    };
+
+    setData((prev) => [todo, ...prev]);
+
+    // clearing input field
+    setInput("");
+  };
+
+  const handleDel = (id) => {
+    const delItem = data.filter((dl) => dl.id !== id);
+    setData(delItem);
+  };
+
+  return (
+    <>
+      <div className="main-container">
+        <h1 style={{ textAlign: "center", color: "white" }}>Todo App</h1>
+        <div className="add-todo-container">
+          <input
+            type="text"
+            placeholder="Add your Todo"
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+          />
+          <button onClick={handleAddTodo}>Add Todo</button>
+        </div>
+        <TodoList
+          data={data}
+          handleDel={handleDel}
+        />
+      </div>
+    </>
+  );
+};
