b81afdf188050522c1e49288eadb49b9ba867434
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-10-07 16:32:00 +0530

    Code of the dau

--- Todo-useState/src/Components/AddTodo.jsx
+++ Todo-useState/src/Components/AddTodo.jsx
@@ -24,48 +24,46 @@ export const AddTodo = () => {
     setInput("");
   };
 
-
   // delete functionality
   const handleDel = (id) => {
     const delItem = data.filter((dl) => dl.id !== id);
     setData(delItem);
   };
 
-
   // Edit functionality
   const handleEdit = (id) => {
-    const editItem = data.map((el)=>{
-        if(el.id === id){
-         return {...el, isEdited: !el.isEdited}
-        }
-        return el;
-      })
-      setData(editItem);
-  }
+    const editItem = data.map((el) => {
+      if (el.id === id) {
+        return { ...el, isEdited: !el.isEdited };
+      }
+      return el;
+    });
+    setData(editItem);
+  };
 
   // handle cancel functionality
   const handleCancel = (id) => {
-    const cancelItem = data.map((cl) => {
-      if(cl.id === id){
-        return {...cl, isEdited: false}
+    const cancleItems = data.map((cl) => {
+      if (cl.id === id) {
+        return { ...cl, isEdited: false };
       }
       return cl;
     });
-    setData(cancelItem);
-  }
-  
+    console.log("🚀 ~ cancelItem:", cancleItems);
+    setData(cancleItems);
+    console.log("🚀 ~ data:", data);
+  };
 
   // handle Confirm functionality
-  const handleConfirm = (id) => {
-    const confirmItem = data.map((cf)=> {
-      if(cf.id === id){
-        return {...cf, isEdited : false}
+  const handleConfirm = (id, updatedData) => {
+    const confirmItem = data.map((cf) => {
+      if (cf.id === id) {
+        return { ...cf, isEdited: false, text: updatedData };
       }
       return cf;
-    })
-    setData(confirmItem)
-  }
-
+    });
+    setData(confirmItem);
+  };
 
   return (
     <>
@@ -84,9 +82,9 @@ export const AddTodo = () => {
           data={data}
           handleDel={handleDel}
           handleEdit={handleEdit}
-          setData = {setData}
-          handleCancel = {handleCancel}
-          handleConfirm = {handleConfirm}
+          setData={setData}
+          handleCancel={handleCancel}
+          handleConfirm={handleConfirm}
         />
       </div>
     </>
