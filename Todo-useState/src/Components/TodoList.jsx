e6e0931926337a9a70210a959821f4d9ba16c535
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-10-07 08:22:00 +0530

    Updated code

--- Todo-useState/src/Components/TodoList.jsx
+++ Todo-useState/src/Components/TodoList.jsx
@@ -1,25 +1,76 @@
-import { FaRegCircleCheck } from "react-icons/fa6";
 import { MdEdit } from "react-icons/md";
 import { MdDeleteForever } from "react-icons/md";
+import { MdCancel } from "react-icons/md";
+import { GiConfirmed } from "react-icons/gi";
 
-import "../App.css"
+import "../App.css";
 
-export const TodoList = ({ data, handleDel,handleEdit }) => {
+export const TodoList = ({
+  data,
+  handleDel,
+  handleEdit,
+  setData,
+  handleCancel,
+  handleConfirm,
+}) => {
   return (
     <>
-      <div className="container" >
+      <div className="container">
         {data.map((el) => (
-          <div key={el.id} className="todo-list-container">
+          <div
+            key={el.id}
+            className="todo-list-container"
+          >
             <div>
-              <FaRegCircleCheck className="check-icon" />
-              <p>{el.text}</p>
-                {
-                  el.isEdited ? <input type="text" value={el.text} style={{textTransform: "capitalize"}} /> : <p>{el.text}</p>
-                }
+              <input
+                type="checkbox"
+                className="check-box"
+              />
+
+              {el.isEdited ? (
+                <div className="modal">
+                  <div className="input-container">
+                    <h4>Edit your Todo</h4>
+                    <input
+                      type="text"
+                      value={el.text}
+                      onChange={(e) => {
+                        const updatedData = data.map((item) =>
+                          item.id === el.id
+                            ? { ...item, text: e.target.value }
+                            : item
+                        );
+                        setData(updatedData);
+                      }}
+                    />
+                  </div>
+                  <div className="btn-container">
+                    <MdCancel
+                      className="cancel-icon"
+                      onClick={() => handleCancel(el.id)}
+                    />
+                    <GiConfirmed
+                      className="confirm-icon"
+                      onClick={() => handleConfirm(el.id)}
+                    />
+                  </div>
+                </div>
+              ) : (
+                <p>{el.text}</p>
+              )}
             </div>
-            <div style={{ display: "flex" }}>
-              <MdEdit className="edit-icon" onClick={()=> handleEdit(el.id)} />
-              <MdDeleteForever className="del-icon" onClick={()=> handleDel(el.id)} />
+            <div
+              style={{ display: "flex" }}
+              className="edit-del-btns"
+            >
+              <MdEdit
+                className="edit-icon"
+                onClick={() => handleEdit(el.id)}
+              />
+              <MdDeleteForever
+                className="del-icon"
+                onClick={() => handleDel(el.id)}
+              />
             </div>
           </div>
         ))}
