c2cebd46bfb2084929c081d00ed097eea24425db
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-10-17 15:16:43 +0530

    Code of the day

--- React-Router/src/Routes/AllRoutes.jsx
+++ React-Router/src/Routes/AllRoutes.jsx
@@ -4,6 +4,8 @@ import { Home } from "../Pages/Home"
 // import { Login } from "../Pages/Login"
 import { NotFoundPage } from "../Pages/NotFoundPage"
 import { Route, Routes } from "react-router-dom"
+import { SinglePage } from "../Pages/SinglePage";
+import { InfoPage } from "../Pages/InfoPage";
 
 export const AllRoutes = () => {
     return (
@@ -12,7 +14,8 @@ export const AllRoutes = () => {
                 <Route path="/" element = {<Home />} ></Route>
                 <Route path="/about" element = {<About />} ></Route>
                 <Route path="/contact" element = {<Contact />} ></Route>
-                {/* <Route path="/login" element = {<Login />} ></Route> */}
+                <Route path="/single_page" element = {<SinglePage />} ></Route>
+                <Route path="/info_page/:userID" element = {<InfoPage />} ></Route>
                 <Route path= "*" element ={<NotFoundPage />}></Route>
             </Routes>
         </>
