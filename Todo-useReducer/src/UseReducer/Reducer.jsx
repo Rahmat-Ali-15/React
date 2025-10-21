55bb9b88ab779ddec45aaef83b584fe2f47794b2
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-10-21 21:27:08 +0530

    Code of the day

--- Todo-useReducer/src/UseReducer/Reducer.jsx
+++ Todo-useReducer/src/UseReducer/Reducer.jsx
@@ -1,17 +1,21 @@
 import { ADD_TODO, DELETE_TODO, EDIT_TODO } from "./Action";
 
 export const Reducer = (state, action) => {
-    switch(action.type){
-        case ADD_TODO:
-            return [...state, action.payload ];
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, action.payload];
 
-        case EDIT_TODO:
-            return [...state, action.payload];
+    case EDIT_TODO:
+      return state.map((el) =>
+        el.id === action.payload.id
+          ? { ...el, todoText: action.payload.todoText }
+          : el
+      );
 
-        case DELETE_TODO:
-            return [...state, action.payload];
+    case DELETE_TODO:
+      return state.filter((el) => el.id !== action.payload);
 
-        default:
-            return state;
-    }
-}
+    default:
+      return state;
+  }
+};
