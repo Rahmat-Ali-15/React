ea35405af284d9a23e01d45181cfe0425649dde3
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-10-23 20:17:21 +0530

    Code of the day

--- Axiox/src/Components/Todo.jsx
+++ Axiox/src/Components/Todo.jsx
@@ -1,43 +1,52 @@
 import axios from "axios";
-import { useState,useRef } from "react";
-import {nanoid} from "nanoid"
+import { useState, useRef } from "react";
+import { nanoid } from "nanoid";
 import { TodoList } from "./TodoList";
 
 export const Todo = () => {
+  const [count, setCount] = useState(0);
 
-    const [count, setCount] = useState(0)
+  // let api = `https://api-server-zecj.onrender.com/todo`;
+  let api = `https://api-database-1.onrender.com/Todo`;
 
-    // let api = `https://api-server-zecj.onrender.com/todo`;
-    let api = `https://api-database-1.onrender.com/Todo`;
+  const dataRef = useRef(null);
+  console.log("🚀 ~ Todo ~ dataRef:", dataRef)
 
-    const dataRef = useRef(null);
+  const handleClick = async () => {
+    const values = dataRef.current.value;
 
-    const handleClick = async () => {
-        const values = dataRef.current.value;
+    let obj = {
+      id: nanoid(),
+      todo: values,
+      isEdit: false,
+      isCompleted: false,
+    };
 
-        let obj = {
-            id : nanoid(),
-            todo : values,
-            isEdited : false,
-            isCompleted : false
-        }
-
-        try {
-            let res = await axios.post(api,obj);
-            setCount((prev) => prev + 1)
-            console.log(res)
-        } catch (error) {
-            console.log(error)
-        }
-
+    try {
+      let res = await axios.post(api, obj);
+      setCount((prev) => prev + 1);
+      console.log(res, "this is res");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return(
-        <>
-            <h1>Todo</h1>
-            <input type="text" name="addTodo" id="" placeholder="add todo..." ref={dataRef} />
-            <input type="button" value="Add Todo" onClick = {handleClick} />
-            <TodoList props={count} />
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Todo</h1>
+      <input
+        type="text"
+        name="addTodo"
+        id=""
+        placeholder="add todo..."
+        ref={dataRef}
+      />
+      <input
+        type="button"
+        value="Add Todo"
+        onClick={handleClick}
+      />
+      <TodoList props={count} />
+    </>
+  );
+};
