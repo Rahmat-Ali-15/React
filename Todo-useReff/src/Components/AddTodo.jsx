66f8bb4b83e713d236a086ff6e79dc831485e54d
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-04 23:35:34 +0530

    Modified code

--- Todo-useReff/src/Components/AddTodo.jsx
+++ Todo-useReff/src/Components/AddTodo.jsx
@@ -1,28 +1,31 @@
-import React, { useRef } from 'react'
+import React, { useRef } from "react";
+import { useState } from "react";
 
 export const AddTodo = () => {
-    const data = useRef(null);
-
-    const handleTodo = () => {
-        if (data.current.value.trim() === "") return;
-
-        let todo = {
-            id : Date.now(),
-            todo : data.current.value,
-            isEdited : false,
-            isComplete : false
-        };
-        localStorage.setItem("todos", JSON.stringify(todo));
-    }
-
+  const data = useRef(null);
+  const [value, setValue] = useState([]);
+  const handleTodo = () => {
+    if(data.current.value.trim() === "" ) return
+    let todo = {
+      id: Date.now(),
+      todo: data.current.value,
+      isEdited: false,
+      isComplete: false,
+    };
+    setValue((prev) => [...prev, todo]);
+    data.current.value = ("")
+  };
+  console.log("🚀 ~ value:", value);
+  
   return (
     <>
-      
-        <h1>todo</h1>
-        <input type="text" placeholder='Enter the todo...' ref={data} />
-        <button onClick={handleTodo}>Add Todo</button>
-
+      <h1>todo</h1>
+      <input
+        type="text"
+        placeholder="Enter the todo..."
+        ref={data}
+      />
+      <button onClick={handleTodo}>Add Todo</button>
     </>
-  )
-}
-
+  );
+};
