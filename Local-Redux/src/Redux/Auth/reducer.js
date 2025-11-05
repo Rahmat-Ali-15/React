2de06db79ef01ad38c52fbf3df9b9bbcaa9d8ee3
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-05 16:27:51 +0530

    Code of the day

--- Local-Redux/src/Redux/Auth/reducer.js
+++ Local-Redux/src/Redux/Auth/reducer.js
@@ -1,14 +1,28 @@
-import { LOGIN_SUCCESS } from "./action";
+import { LocalData } from "../../Local_Storage/LocalStorage";
+import { LOGIN_FAILURE, LOGIN_SUCCESS } from "./action";
+
+
+const token = LocalData("token");
+
 
 const initialState = {
-  todos: [],
+  isAuth : token ? true : false,
+  token : token || "",
 };
 
 export const AuthReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case LOGIN_SUCCESS:
       return {
-        
+        ...state,
+        isAuth: true,
+        token: payload,
+      };
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        isAuth: false,
+        token: payload,
       };
 
     default:
