2d79eada596bffdb2ee16f7d74f618e59a466900
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-05 16:30:35 +0530

    Code of the day

--- Local-Redux/src/main.jsx
+++ Local-Redux/src/main.jsx
@@ -1,10 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import {App} from './App.jsx'
-import { Provider } from 'react-redux'
-import { myStore } from './Redux/Store.js'
+import { createRoot } from "react-dom/client";
+import { App } from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <Provider store={myStore}>
-    <App />
-  </Provider>,
+import { Provider } from "react-redux";
+import { myStore } from "./Redux/Store.js";
+import { BrowserRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Provider store={myStore}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
