75a27584e8eaaa275b06fa47438eae3a6fa6e15d
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-13 17:26:05 +0530

    Code of the day

--- Redux-Intro(Todo)/src/Redux/Reducer.js
+++ Redux-Intro(Todo)/src/Redux/Reducer.js
@@ -3,7 +3,6 @@ import { ADD_TODO } from "./Action";
 export const initialVal = [];
 
 export const Reducer = (state,action) => {
-    console.log("🚀 ~ action:", action);
     console.log("🚀 ~ state:", state);
     switch(action.type){
         case ADD_TODO:
