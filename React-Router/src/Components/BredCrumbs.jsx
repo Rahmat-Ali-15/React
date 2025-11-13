8bae1bde77287e530e10a8005aa9d988ed3bd9af
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-13 17:23:36 +0530

    Code of the day

--- React-Router/src/Components/BredCrumbs.jsx
+++ React-Router/src/Components/BredCrumbs.jsx
@@ -1,21 +1,25 @@
 import { Link, useLocation } from "react-router-dom";
 
 export const BredCrumbs = () => {
-    const location = useLocation();
-    let crumb = "";
-    console.log("🚀 ~ crumb:", crumb);
-    const bredCrumb = location.pathname.split("/").filter((el)=> el != "").map(curr=>{crumb += `${curr}`
-        return(
-            <div>
-                    <Link to={crumb} >{curr}</Link>
-                </div>
-        )
+  const location = useLocation();
+  let crumb = "";
+  console.log("🚀 ~ crumb:", crumb);
+  const bredCrumb = location.pathname
+    .split("/")
+    .filter((el) => el != "")
+    .map((curr) => {
+      crumb += `${curr}`;
+      return (
+        <div className="crumb">
+          <Link to={crumb}>{curr}</Link>
+        </div>
+      );
     });
-    console.log(bredCrumb);
-    
-    return (
-        <>
-            <div className="crumbs">{crumb}</div>
-        </>
-    )
-}
+  console.log(bredCrumb);
+
+  return (
+    <>
+      <div className="crumbs">{bredCrumb}</div>
+    </>
+  );
+};
