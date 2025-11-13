7bb026f3dfc88ddff55871eb11080da66638e3af
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-13 17:25:09 +0530

    Code of the day

--- Redux-Intro(Todo)/src/Components/Todo.jsx
+++ Redux-Intro(Todo)/src/Components/Todo.jsx
@@ -2,6 +2,7 @@
 import { useRef } from "react"
 import { useDispatch, useSelector } from "react-redux";
 import { ADD_TODO } from "../Redux/Action";
+import { TodoList } from "./TodoList";
 
 export const Todo = () => {
     const refData = useRef(null);
@@ -10,14 +11,19 @@ export const Todo = () => {
 
     const handleClick = () => {
         let val = refData.current.value;
+        if(val==="")return
         dispatch({type: ADD_TODO, payload: val});
+
+        refData.current.value= ""
     }
 
 
     return(
         <>
+        <h1>Add Todo</h1>
             <input type="text" ref={refData} />
             <button onClick={handleClick}>Add Todo</button>
+            <TodoList />
         </>
     )
 }
