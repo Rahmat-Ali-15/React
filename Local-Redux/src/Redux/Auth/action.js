56d1c50dd36e007b443262966cf446548c9dac90
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-16 19:36:21 +0530

    Code of the day

--- Local-Redux/src/Redux/Auth/action.js
+++ Local-Redux/src/Redux/Auth/action.js
@@ -1,2 +1,29 @@
+import axios from "axios";
+
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
+export const LOGIN_REQUEST = "LOGIN_REQUEST";
+export const REMOVE_TOKEN = "REMOVE_TOKEN";
+
+export const LoginUser = (payload) => (dispatch) => {
+  const { email, password } = payload;
+  dispatch({ type: LOGIN_REQUEST });
+  axios
+    .post(
+      "https://reqres.in/api/login",
+      { email, password },
+      {
+        headers: {
+          "x-api-key": "reqres-free-v1",
+          "Content-Type:": "application/json",
+        },
+      }
+    )
+    .then((res) => {
+      console.log("🚀 ~ res:", res);
+      dispatch({ type: LOGIN_SUCCESS, payload: res.data.token });
+    })
+    .catch((err) => {
+      dispatch({ type: LOGIN_FAILURE, payload: err.message });
+    });
+};
