74e3fe807b06718869cb280340955a46f977a78b
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-16 19:36:36 +0530

    Code of the day

--- Local-Redux/src/Redux/Auth/reducer.js
+++ Local-Redux/src/Redux/Auth/reducer.js
@@ -1,30 +1,67 @@
-import { LocalData, SaveData } from "../../Local_Storage/LocalStorage";
-import { LOGIN_FAILURE, LOGIN_SUCCESS } from "./action";
+import {
+  delete_token,
+  LocalData,
+  SaveData,
+} from "../../Local_Storage/LocalStorage";
+import {
+  LOGIN_FAILURE,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  REMOVE_TOKEN,
+} from "./action";
 
+let token_var = "token"
 
 const token = LocalData("token");
 
-
 const initialState = {
-  isAuth : token ? true : false,
-  token : token || "",
+  isAuth: token ? true : false,
+  token: token || "",
+  isError: false,
+  isLoading: false
 };
 
 export const AuthReducer = (state = initialState, { type, payload }) => {
+  console.log("🚀 ~ state:", state);
+  console.log("🚀 ~ payload:", payload);
   switch (type) {
-    case LOGIN_SUCCESS:
-      SaveData("token", payload)
+    case REMOVE_TOKEN: {
+      delete_token(token_var);
+      return {
+        ...state,
+        isLoading: false,
+        isAuth: false,
+        token: "",
+      };
+    }
+    case LOGIN_SUCCESS: {
+      SaveData("token", payload);
       return {
         ...state,
         isAuth: true,
         token: payload,
+        isLoading: true,
+        isError: false,
       };
-    case LOGIN_FAILURE:
+    }
+
+    case LOGIN_FAILURE: {
       return {
         ...state,
         isAuth: false,
-        token: payload,
+        token: "",
+        isError: true,
+        isLoading: false,
       };
+    }
+
+    case LOGIN_REQUEST: {
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    }
 
     default:
       return state;
