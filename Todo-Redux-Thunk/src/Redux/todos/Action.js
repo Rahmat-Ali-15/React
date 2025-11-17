ed8a5b600ed8ff7437f75e87ad15b8a527ff33fb
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-17 16:23:15 +0530

    Code of the day

--- Todo-Redux-Thunk/src/Redux/todos/Action.js
+++ Todo-Redux-Thunk/src/Redux/todos/Action.js
@@ -1,8 +1,41 @@
 import * as types from "../todos/ActionsTypes"
 
-export const AddTodos = (payload) => {
+export const getRequestTodo = () => {
     return {
-        type: types.ADD_TODO_SUCCESS,
+        type: types.GET_TODO_REQUEST
+    }
+}
+
+export const getSuccessTodo = (payload) => {
+    return {
+        type: types.GET_TODO_SUCCESS,
         payload: payload
     };
 };
+export const getFailureTodo = (payload) => {
+    return {
+        type: types.GET_TODO_FAILURE,
+        payload: payload
+    };
+};
+
+// Here adding the data into api
+
+export const addTodoRequest = () => {
+    return {
+        type: types.ADD_TODO_REQUEST,
+    };
+};
+
+export const addTodoFailure = (payload) => {
+    return {
+        type: types.ADD_TODO_FAILURE,
+        payload: payload
+    }
+}
+export const addTodoSuccess = (payload) => {
+    return {
+        type: types.ADD_TODO_SUCCESS,
+        payload: payload
+    }
+}
