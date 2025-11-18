81c6de521c851f40f8bae78b7e43f7725d3750e4
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-18 08:55:45 +0530

    Code of the day

--- Todo-Redux-Thunk/src/components/TodoList.jsx
+++ Todo-Redux-Thunk/src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   getFailureTodo,
   getRequestTodo,
@@ -8,10 +8,21 @@ import axios from "axios";
 import { useEffect } from "react";
 
 const API = import.meta.env.VITE_API;
-console.log("🚀 ~ API:", API);
 
 export const TodoList = () => {
   const dispatch = useDispatch();
+
+  const { todos } = useSelector((state) => state.todo);
+
+  // const handleEdit = (id) => {
+  //   const updateTodo = todos.map((el)=> {
+  //     if(el.id === id){
+  //       return {...el, isEdit: true}
+  //     }
+  //     return el
+  //   })
+  //   dispatch(addTodoSuccess(updateTodo))
+  // }
 
   const getApiCall = () => {
     dispatch(getRequestTodo());
@@ -24,11 +35,32 @@ export const TodoList = () => {
   useEffect(() => {
     console.log("I am invoked");
     getApiCall;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <>
       <h1>Todo List</h1>
+      {todos.map((el) => (
+        <div
+          key={el.id}
+          style={{
+            width: "300px",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            gap: "20px",
+          }}
+        >
+          <p>{el.text}</p>
+          <p>{el.id}</p>
+          <div>
+            {/* <button onClick={()=> handleEdit(el.id)}>Edit</button> */}
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+        </div>
+      ))}
     </>
   );
 };
