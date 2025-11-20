1c0d37a9c7fd60fe4619ea087e25e98f65045445
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-20 14:57:05 +0530

    Code of the day

--- Todo-Redux-Thunk/src/components/TodoList.jsx
+++ Todo-Redux-Thunk/src/components/TodoList.jsx
@@ -28,69 +28,102 @@ export const TodoList = () => {
       .catch((err) => dispatch(getFailureTodo(err)));
   };
 
-  useEffect(() => {
-    getApiCall();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   const handleEdit = (id) => {
     dispatch(editTodoRequest());
+    const updateEdit = todos
+      .map((el) => (el.id === id ? { ...el, isEdit: !el.isEdit } : null))
+      .filter((el) => el !== null);
+    console.log("🚀 ~ updateEdit:", updateEdit);
 
-    // Find the specific todo that's being edited
-    const todoToUpdate = todos.find((el) => el.id === id);
-    if (!todoToUpdate) {
-      dispatch(editTodoFailure(new Error("Todo not found")));
-      return;
-    }
-
-    // Create updated todo with toggled isEdit
-    const updatedTodo = {
-      ...todoToUpdate,
-      isEdit: !todoToUpdate.isEdit,
-    };
-
-    // Update local state
-    const updatedTodos = todos.map((el) => (el.id === id ? updatedTodo : el));
-
-    // Send ONLY the specific todo to update, not the entire array
     axios
-      .patch(`${API}/${id}`, updatedTodo)
-      .then(() => {
-        dispatch(editTodoSuccess(updatedTodos));
+      .patch(`${API}/${id}`, ...updateEdit)
+      .then((res) => {
+        dispatch(editTodoSuccess([id, res.data]));
       })
       .catch((err) => {
-        console.error("Edit failed:", err);
         dispatch(editTodoFailure(err));
       });
   };
 
   const handleDelete = (id) => {
     dispatch(deleteTodoRequest());
-
-    const updateTodo = todos.filter((dl) => dl.id !== id);
-
+    let deleteItem = todos.filter((dl) => dl.id !== id);
     axios
       .delete(`${API}/${id}`)
-      .then(() => {
-        dispatch(deleteTodoSuccess(updateTodo));
+      .then((res) => {
+        console.log("res", res);
+        dispatch(deleteTodoSuccess(deleteItem));
       })
       .catch((err) => {
         dispatch(deleteTodoFailure(err));
       });
   };
 
-  const hadnleCancel = (id) => {
-    dispatch(editTodoRequest());
-    const cancelItem = todos.map((cl) =>
-      cl.id === id ? { ...cl, isEdit: !cl.isEdit } : cl
-    );
-    axios
-      .patch(`${API}/${id}`, {isEdit: false})
-      .then(() => {
-        return dispatch(editTodoSuccess(cancelItem));
-      })
-      .catch((err) => dispatch(editTodoFailure(err)));
-  };
+  // Handle edit 2nd logic
+
+  // const handleEdit = (id) => {
+  //   dispatch(editTodoRequest());
+
+  //   // Find the specific todo that's being edited
+  //   const todoToUpdate = todos.find((el) => el.id === id);
+  //   if (!todoToUpdate) {
+  //     dispatch(editTodoFailure(new Error("Todo not found")));
+  //     return;
+  //   }
+
+  //   // Create updated todo with toggled isEdit
+  //   const updatedTodo = {
+  //     ...todoToUpdate,
+  //     isEdit: !todoToUpdate.isEdit,
+  //   };
+
+  //   // Update local state
+  //   const updatedTodos = todos.map((el) => (el.id === id ? updatedTodo : el));
+
+  //   // Send ONLY the specific todo to update, not the entire array
+  //   axios
+  //     .patch(`${API}/${id}`, updatedTodo)
+  //     .then(() => {
+  //       dispatch(editTodoSuccess(updatedTodos));
+  //     })
+  //     .catch((err) => {
+  //       console.error("Edit failed:", err);
+  //       dispatch(editTodoFailure(err));
+  //     });
+  // };
+
+  // const handleDelete = (id) => {
+  //   dispatch(deleteTodoRequest());
+
+  //   const updateTodo = todos.filter((dl) => dl.id !== id);
+
+  //   axios
+  //     .delete(`${API}/${id}`)
+  //     .then(() => {
+  //       dispatch(deleteTodoSuccess(updateTodo));
+  //     })
+  //     .catch((err) => {
+  //       dispatch(deleteTodoFailure(err));
+  //     });
+  // };
+
+  // const hadnleCancel = (id) => {
+  //   dispatch(editTodoRequest());
+  //   const cancelItem = todos.map((cl) =>
+  //     cl.id === id ? { ...cl, isEdit: !cl.isEdit } : cl
+  //   );
+  //   axios
+  //     .patch(`${API}/${id}`, { isEdit: false })
+  //     .then(() => {
+  //       return dispatch(editTodoSuccess(cancelItem));
+  //     })
+  //     .catch((err) => dispatch(editTodoFailure(err)));
+  // };
+
+  useEffect(() => {
+    getApiCall();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   if (isLoading) {
     return <h5>Loading...</h5>;
@@ -100,7 +133,7 @@ export const TodoList = () => {
     <>
       <h1>Todo List</h1>
       {isError && <h1>Something went wrong...❌</h1>}
-      {todos.length > 0 &&
+      {/* {todos.length > 0 &&
         todos.map((el) => (
           <div
             key={el.id}
@@ -133,7 +166,40 @@ export const TodoList = () => {
               </>
             )}
           </div>
-        ))}
+        ))} */}
+      {todos.map((el) => {
+        return (
+          <div
+            key={el.id}
+            style={{
+              width: "500px",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              gap: "20px",
+            }}
+          >
+            <input type="checkbox" />
+            <p>{el.text}</p>
+
+            {el.isEdit && el.isEdit ? (
+              <>
+                <div>
+                  <button>Cancel</button>
+                  <button>Delete</button>
+                </div>
+              </>
+            ) : (
+              <>
+                <div>
+                  <button onClick={() => handleEdit(el.id)}>Edit</button>
+                  <button onClick={() => handleDelete(el.id)}>Delete</button>
+                </div>
+              </>
+            )}
+          </div>
+        );
+      })}
     </>
   );
 };
