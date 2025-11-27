b99ca9ce805578b482c017d9a69e6ddb8f05a750
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-27 20:28:01 +0530

    Code of the day

--- React-Router/src/Routes/AllRoutes.jsx
+++ React-Router/src/Routes/AllRoutes.jsx
@@ -1,23 +1,56 @@
 import { About } from "../Pages/About";
-import { Contact } from "../Pages/Contact"
-import { Home } from "../Pages/Home"
+import { Contact } from "../Pages/Contact";
+import { Home } from "../Pages/Home";
 // import { Login } from "../Pages/Login"
-import { NotFoundPage } from "../Pages/NotFoundPage"
-import { Route, Routes } from "react-router-dom"
+import { NotFoundPage } from "../Pages/NotFoundPage";
+import { Route, Routes } from "react-router-dom";
 import { SinglePage } from "../Pages/SinglePage";
 import { InfoPage } from "../Pages/InfoPage";
+import { ShowLogin } from "../Pages/ShowLogin";
+import { Auth } from "../Components/Auth";
+import { PrivateRoute } from "../Auth/PrivateRoute";
 
 export const AllRoutes = () => {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element = {<Home />} ></Route>
-                <Route path="/about" element = {<About />} ></Route>
-                <Route path="/contact" element = {<Contact />} ></Route>
-                <Route path="/single_page" element = {<SinglePage />} ></Route>
-                <Route path="/info_page/:userID" element = {<InfoPage />} ></Route>
-                <Route path= "*" element ={<NotFoundPage />}></Route>
-            </Routes>
-        </>
-    )
-}
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        ></Route>
+        <Route
+          path="/auth"
+          element={<Auth />}
+        ></Route>
+        <Route
+          path="/show_login"
+          element={<ShowLogin />}
+        ></Route>
+        <Route
+          path="/about"
+          element={<About />}
+        ></Route>
+        <Route
+          path="/contact"
+          element={<Contact />}
+        ></Route>
+        <Route
+          path="/single_page"
+          element={
+            <PrivateRoute>
+              <SinglePage />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/info_page/:userID"
+          element={<InfoPage />}
+        ></Route>
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        ></Route>
+      </Routes>
+    </>
+  );
+};
