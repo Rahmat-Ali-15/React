67156eaa9d074cac231c1719398b9c9d2e776d21
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-27 20:28:16 +0530

    Code of the day

--- React-Router/src/main.jsx
+++ React-Router/src/main.jsx
@@ -1,9 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import {App} from './App.jsx'
-import {BrowserRouter} from "react-router-dom"
+import { createRoot } from "react-dom/client";
+import { App } from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter } from "react-router-dom";
+import { ContextComponent } from "./Context/ContextComponent.jsx";
+
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <ContextComponent>
+      <App />
+    </ContextComponent>
+  </BrowserRouter>
+);
