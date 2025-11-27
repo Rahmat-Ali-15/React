fd2d02ffdaef36cd772d2650d38f7d39315502ca
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-27 20:31:01 +0530

    Code of the day

--- Todo-Redux-Thunk/src/Redux/auth/Reducer.js
+++ Todo-Redux-Thunk/src/Redux/auth/Reducer.js
@@ -1,41 +1,71 @@
-import { loadData } from "../../utils/localStorage";
+// import { loadData } from "../../utils/localStorage";
 import * as abrakadabra from "./ActionsTypes";
 
+// const token = loadData("token");
+// let VITE_API = "https://api-server-zecj.onrender.com/todo";
 
-const token = loadData("token")
+// const initialState = {
+//   token: token || "",
+//   isAuth: false,
+//   isLoading: false,
+//   isError: false,
+// };
 
 const initialState = {
-    token: token || "",
-    isAuth: false,
-    isLoading: false,
-    isError: false
-}
+  user: [],
+  isAuth: false,
+  isLoading: false,
+  isError: false,
+};
 
-export const authReducer = (oldState= initialState, {type, payload}) => {
-    switch(type){
-        case abrakadabra.GET_LOGIN_REQUEST: {
-            return {
-                ...oldState,
-                isLoading: true,
-                isError: false
-            }
-        }
-        case abrakadabra.GET_LOGIN_SUCCESS: {
-            return {
-                ...oldState,
-                isLoading: false,
-                isError: false,
-                todos: [...oldState.todos, payload]
-            }
-        }
-        case abrakadabra.GET_LOGIN_FAILURE: {
-            return {
-                ...oldState,
-                isLoading: false,
-                isError: true,
-            }
-        }
-        default:
-          return  oldState
+export const authReducer = (oldState = initialState, { type, payload }) => {
+  switch (type) {
+    case abrakadabra.GET_SIGNUP_REQUEST: {
+      return {
+        ...oldState,
+        isLoading: true,
+        isError: false,
+      };
     }
-}
+    case abrakadabra.GET_SIGNUP_SUCCESS: {
+      return {
+        ...oldState,
+        isLoading: false,
+        isError: false,
+        user: [...oldState.user, payload],
+      };
+    }
+    case abrakadabra.GET_SIGNUP_FAILURE: {
+      return {
+        ...oldState,
+        isLoading: false,
+        isError: true,
+      };
+    }
+
+    case abrakadabra.GET_LOGIN_REQUEST: {
+      return {
+        ...oldState,
+        isLoading: true,
+        isError: false,
+      };
+    }
+    case abrakadabra.GET_LOGIN_SUCCESS: {
+      return {
+        ...oldState,
+        isLoading: false,
+        isError: false,
+        user: [...oldState.user, payload],
+      };
+    }
+    case abrakadabra.GET_LOGIN_FAILURE: {
+      return {
+        ...oldState,
+        isLoading: false,
+        isError: true,
+      };
+    }
+    default:
+      return oldState;
+  }
+};
