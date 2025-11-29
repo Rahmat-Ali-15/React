8559e8c8757110c637e52860c86fb1c798654ac5
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-29 18:54:54 +0530

    Code of the day

--- Todo-Redux-Thunk/src/main.jsx
+++ Todo-Redux-Thunk/src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 import { Provider } from "react-redux";
 import { ownStore } from "./Redux/Store.js";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={ownStore}>
-    <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={ownStore}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
