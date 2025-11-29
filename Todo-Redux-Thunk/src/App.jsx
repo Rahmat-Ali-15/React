9d58fa008d0df136c3e37eb34d7b691f65190a41
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-29 18:53:53 +0530

    Code of the day

--- Todo-Redux-Thunk/src/App.jsx
+++ Todo-Redux-Thunk/src/App.jsx
@@ -1,14 +1,17 @@
-import './App.css'
-import { AddTodo } from './components/AddTodo'
-import { TodoList } from './components/TodoList'
+import "./App.css";
+import { AddTodo } from "./components/AddTodo";
+import { Navbar } from "./components/Navbar";
+import { TodoList } from "./components/TodoList";
+import { AllRoutes } from "./Routes/AllRoutes";
 
 export function App() {
-
   return (
     <>
-      <h1>Redux todos</h1>
+      {/* <h1>Redux todos</h1>
       <AddTodo />
-      <TodoList/>
+      <TodoList/> */}
+      <Navbar />
+      <AllRoutes />
     </>
-  )
+  );
 }
