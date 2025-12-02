c2e16cd01bd89c86777c2bdd9d1bc7e8564096a9
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-12-02 09:06:49 +0530

    Basec react-router

---
+++ React-Router-Practice/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import { Navbar } from "./Components/Navbar";
+import { AllRoutes } from "./Routes/AllRoutes";
+
+export const App = () => {
+  return <>
+    <Navbar />
+    <AllRoutes />
+  </>;
+};
