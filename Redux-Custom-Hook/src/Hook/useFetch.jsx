190fff62e7834452484ed7834a3fcda39925dc73
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-12-08 18:54:47 +0530

    Code of the day

--- Redux-Custom-Hook/src/Hook/useFetch.jsx
+++ Redux-Custom-Hook/src/Hook/useFetch.jsx
@@ -1,40 +1,47 @@
-import { useEffect, useReducer } from "react"
+import { useEffect, useReducer } from "react";
 
-const  ACTIONS = {
-    API_REQUEST: "api-request",
-    FETCH_DATA: "fetch-data",
-    ERROR: "error"
-}
+const ACTIONS = {
+  API_REQUEST: "api-request",
+  FETCH_DATA: "fetch-data",
+  ERROR: "error",
+};
 
 const initialState = {
-    data: [],
-    error: null,
-    loading: false
-}
+  data: [],
+  error: null,
+  loading: false,
+};
 
 const reducer = (state, action) => {
-    switch (action.typpe){
-        case ACTIONS.API_REQUEST:
-            return {...state, data: [], loading: true}
+  switch (action.typpe) {
+    case ACTIONS.API_REQUEST:
+      return { ...state, data: [], loading: true };
 
-        case ACTIONS.FETCH_DATA:
-            return {...state, data: action.payload, loading: false, error: null}
+    case ACTIONS.FETCH_DATA:
+      return { ...state, data: action.payload, loading: false, error: null };
 
-        case ACTIONS.ERROR:
-            return {...state, data: [], error: action.payload}
+    case ACTIONS.ERROR:
+      return { ...state, data: [], error: action.payload };
 
-        default:
-            return state
-    }
-}
+    default:
+      return state;
+  }
+};
 
 // custom fetch hook
 export const useFetch = (url) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    useEffect(()=>{
-        dispatch({type: ACTIONS.API_REQUEST});
-        fetch(url).then((res)=> res.json()).then((res)=>{dispatch({type: ACTIONS.FETCH_DATA, payload: res})}).catch((err)=> {dispatch({type: ACTIONS.ERROR, payload: err})})
-    },[url])
-    return state
-}
+  useEffect(() => {
+    dispatch({ type: ACTIONS.API_REQUEST });
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        dispatch({ type: ACTIONS.FETCH_DATA, payload: res });
+      })
+      .catch((err) => {
+        dispatch({ type: ACTIONS.ERROR, payload: err });
+      });
+  }, [url]);
+  return state;
+};
