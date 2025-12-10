99f0a7423309bf7f1860fc5674e1afe11ec49bb0
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-12-10 17:40:15 +0530

    Code of the day

---
+++ Custom-Component/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { InputBox } from './Components/InputBox'
+
+export function App() {
+
+  return (
+    <>
+      <InputBox label = "hello" length ={4} perBox={1} />
+    </>
+  )
+}
