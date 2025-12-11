328264664fccec34b529a06baeb324da5ceaf6b3
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-12-11 09:20:42 +0530

    Code of the day

--- Custom-Component/src/Components/PinItems.jsx
+++ Custom-Component/src/Components/PinItems.jsx
@@ -2,21 +2,26 @@ import PropTypes from "prop-types";
 import React from "react";
 
 const style = {
-  width: "50px",
-  height: "50px",
+  width: "70px",
+  height: "70px",
   border: "2px solid gray",
   margin: "10px",
   padding: "10px",
   textAlign: "center",
+  borderRadius: "10px",
 };
 
-export const PinItems = React.forwardRef(({ max, onChange }, ref) => {
+export const PinItems = React.forwardRef(({ max, onChange, onBackspace, onPaste }, ref) => {
   const handleKeyDown = (e) => {
     console.log("🚀 ~ e:", e);
 
     switch (e.keyCode) {
-      // case 8:
-      //     return onchange()
+      case 8:
+          // return onchange()
+      if (e.target.value === "") {
+        onBackspace();
+      }
+      break
 
       case 9:
         // for tab behave default
@@ -28,14 +33,22 @@ export const PinItems = React.forwardRef(({ max, onChange }, ref) => {
     }
   };
 
+  const handlePaste = (e) => {
+      e.preventDefault();
+      const pasted = e.clipboardData.getData("text");
+      onPaste(pasted);
+    };
+
   return (
     <>
       <input
+        className="input-box"
         ref={ref}
         type="text"
         style={style}
         maxLength={max}
         onKeyUp={handleKeyDown}
+        onPaste={handlePaste}
       />
     </>
   );
