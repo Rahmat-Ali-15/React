3bc1b83f5f18bdfccd868cf629920b72500b9507
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-12-12 20:46:30 +0530

    Added logic for buttons

--- Redux-Toolkit/src/Reducer/TodoReducer.js
+++ Redux-Toolkit/src/Reducer/TodoReducer.js
@@ -16,11 +16,31 @@ const todoReducer = createSlice({
     removeTodo(state, action) {
       state.item = state.item.filter((el) => el.id !== action.payload);
     },
-    editTodo(state,action){
-        state.item = state.item.map(el=> el.id === action.payload ? {...el, isEdited: !el.isEdited}: el)
-    }
+    editTodo(state, action) {
+      state.item = state.item.map((el) =>
+        el.id === action.payload ? { ...el, isEdited: !el.isEdited } : el
+      );
+    },
+    cancelTodo(state, action) {
+      state.item = state.item.map((el) =>
+        el.id === action.payload ? { ...el, isEdited: !el.isEdited } : el
+      );
+    },
+    confirmTodo(state, action) {
+      const { id, updatedData } = action.payload;
+
+      state.item = state.item.map((el) =>
+        el.id === id ? { ...el, text: updatedData, isEdited: false } : el
+      );
+    },
+    toggleCheckbox(state, action) {
+      state.item = state.item.map((el) =>
+        el.id === action.payload ? { ...el, isCompleted: !el.isCompleted } : el
+      );
+    },
   },
 });
 
-export const { addTodos, removeTodo,editTodo } = todoReducer.actions;
+export const { addTodos, removeTodo, editTodo, cancelTodo, confirmTodo, toggleCheckbox } =
+  todoReducer.actions;
 export default todoReducer.reducer;
