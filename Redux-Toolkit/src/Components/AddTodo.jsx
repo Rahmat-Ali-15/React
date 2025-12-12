ae84a3a8f9f2c4f2f218d844ccf4244cc44bfbc0
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-12-12 20:45:41 +0530

    Modified code

--- Redux-Toolkit/src/Components/AddTodo.jsx
+++ Redux-Toolkit/src/Components/AddTodo.jsx
@@ -7,18 +7,29 @@ export const AddTodo = () => {
   const inputVal = useRef(null);
 
   const handleAdd = () => {
+
+    if(inputVal.current.value.trim() === "") return
+    if(inputVal.current.value.length > 20){
+      alert("Please Enter character between 1-20");
+      return
+    }
+
     const value = inputVal.current.value;
     dispatch(addTodos(value));
+
+    inputVal.current.value = ""
   };
 
   return (
     <>
-      <input
-        type="text"
-        ref={inputVal}
-        placeholder="Enter todos..."
-      />
-      <button onClick={handleAdd}>Add Todo</button>
+      <div className="addTodo-container">
+        <input
+          type="text"
+          ref={inputVal}
+          placeholder="Enter todos..."
+        />
+        <button onClick={handleAdd}>Add Todo</button>
+      </div>
     </>
   );
 };
