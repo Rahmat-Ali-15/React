55cf44d1f50475257170fd312f018595e2f9fa7d
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-12-20 21:42:45 +0530

    Code of the day

---
+++ useReducer-Hook-3/src/Pages/Todo.jsx
@@ -0,0 +1,11 @@
+import { AddTodo } from "../Components/AddTodo"
+// import { TodoList } from "../Components/TodoList"
+
+export const Todo = () => {
+    return(
+        <>
+            <AddTodo />
+            {/* <TodoList /> */}
+        </>
+    )
+}
