9e5ee6d8070d75f497fe420de1d78c9599c6f5cd
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-01-01 18:37:52 +0530

    Code of the day

--- Redux-Toolkit-From-Scratch-2/src/Store/Store.js
+++ Redux-Toolkit-From-Scratch-2/src/Store/Store.js
@@ -1,6 +1,7 @@
 import {configureStore} from "@reduxjs/toolkit"
-import { myOwnReduceer } from "../Reducer/Reducer"
+// import { myOwnReduceer } from "../Reducer/Reducer"
+import { rootSlices } from "./combineSlice"
 
 export const myOwnStore = configureStore({
-    reducer: myOwnReduceer,
+    reducer: rootSlices,
 })
