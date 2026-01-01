d5fd6853635cda66fe423c6a56c180ee24ade444
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-01-01 18:38:38 +0530

    Code of the day

--- Redux-Toolkit-From-Scratch-2/src/App.jsx
+++ Redux-Toolkit-From-Scratch-2/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import { Counter } from './Components/Counter'
+import { Todos } from './Pages/Todos'
 
 export function App() {
 
   return (
     <>
       <Counter />
+      <br />
+      <br />
+      <Todos />
     </>
   )
 }
