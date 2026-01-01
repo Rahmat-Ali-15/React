e10d1cd0a341cca5f2bb15dd2e57adc971bcd398
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-01-01 18:36:28 +0530

    Code of the day

--- Redux-Toolkit-From-Scratch-2/src/Reducer/Reducer.js
+++ Redux-Toolkit-From-Scratch-2/src/Reducer/Reducer.js
@@ -1,5 +1,10 @@
 /* eslint-disable no-unused-vars */
-import { createReducer } from "@reduxjs/toolkit";
+import {
+  createReducer,
+  createSelector,
+  createSlice,
+  nanoid,
+} from "@reduxjs/toolkit";
 import * as action_type from "./Action";
 
 export const myOwnReduceer = createReducer(
@@ -20,3 +25,65 @@ export const myOwnReduceer = createReducer(
       });
   }
 );
+
+const myOwnTodoReducer = createSlice({
+  name: "todos",
+  initialState: {
+    todoItems: [],
+    pinTodo: [],
+    isLoading: false,
+    isError: false,
+  },
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+        isEdits: false,
+        isComplete: false,
+      };
+      state.todoItems.push(todo);
+    },
+    editTodo: (state, action) => {
+      state.todoItems = state.todoItems.map((el) =>
+        el.id === action.payload ? { ...el, isEdits: !el.isEdits } : el
+      );
+    },
+    deleteTodo: (state, action) => {
+      state.todoItems = state.todoItems.filter((el) => el.id != action.payload);
+    },
+    completeTodo: (state, action) => {
+      state.pinTodo = state.todoItems.map((el) =>
+        el.id === action.payload ? { ...el, isComplete: !el.isComplete } : el
+      );
+      state.todoItems = state.todoItems.map((el) =>
+        el.id === action.payload ? { ...el, isComplete: !el.isComplete } : el
+      ).filter((el) => el.isComplete != true);
+    },
+  },
+  // selectors: {
+  //   completeTodoChecked: (state) =>
+  //     state.todoItems.filter((el) => el.isComplete === true),
+  // },
+  selectors: {
+    completeTodoChecked: createSelector([(state) => state.pinTodo], (pinTodo) => {
+      return pinTodo.filter((el) => el.isComplete === true);
+    }
+    ),
+  },
+});
+
+export const { addTodo, editTodo, deleteTodo, completeTodo } =
+  myOwnTodoReducer.actions;
+
+// const pinItems = (state) => {
+//   return myOwnTodoReducer.selectors.completeTodoChecked(state);
+// };
+
+export const { completeTodoChecked } = myOwnTodoReducer.getSelectors(
+  (state) => {
+    return state.todo;
+  }
+);
+
+export const myTodoReducers = myOwnTodoReducer.reducer;
