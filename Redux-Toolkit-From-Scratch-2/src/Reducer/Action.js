4e6444d08ac8b2f1ec05341985ce89ff00931a66
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-01-05 21:45:06 +0530

    Code of the day

--- Redux-Toolkit-From-Scratch-2/src/Reducer/Action.js
+++ Redux-Toolkit-From-Scratch-2/src/Reducer/Action.js
@@ -1,17 +1,37 @@
-import { createAction } from "@reduxjs/toolkit"
+import { createAction, createAsyncThunk } from "@reduxjs/toolkit";
+import { api } from "../Api/Api";
 
-export const increment = createAction("counter/increment")
-export const decrement = createAction("counter/decrement")
-export const incrementByValue = createAction("counter/incrementByValue")
-export const unknown = createAction("counter/unknown")
+export const increment = createAction("counter/increment");
+export const decrement = createAction("counter/decrement");
+export const incrementByValue = createAction("counter/incrementByValue");
+export const unknown = createAction("counter/unknown");
 
-export const randomNumber = createAction("random/number")
+export const randomNumber = createAction("random/number");
 
 export const isActionWithNumberPayload = (action) => {
-    // console.log("🚀 ~ action:", action);
+  // console.log("🚀 ~ action:", action);
 
-    // It's always return bool value
-    return typeof action.payload === "number"
-}
+  // It's always return bool value
+  return typeof action.payload === "number";
+};
 
 //? This is anction p age we only use in createReducer
+
+
+
+// Here we are doing with HTTPS method
+
+// ? READ
+
+export const fetchTodos = createAsyncThunk("createTodo", async () => {
+  const res = await api.post("/todo");
+  return res.data;
+});
+
+// # CREAT
+
+export const addTodoHttp = createAsyncThunk("addTodo", async (newPost) => {
+  console.log("🚀 ~ newPost:", newPost);
+  const res = await api.post("/todo", newPost);
+  return res.data;
+});
